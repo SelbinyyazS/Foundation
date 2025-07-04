c62b1d65a79ac06303de0470213e399686154610
Selbinyyaz Sultanova <sultanovaselvi31@gmail.com>
2025-07-04 22:10:14 +0300

    core/settings.py typo correction

--- core/settings.py
+++ core/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-     'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
