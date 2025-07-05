a4f03c648233bd20ffdcca09b62cec7e0ed34957
Selbinyyaz Sultanova <sultanovaselvi31@gmail.com>
2025-07-05 16:36:16 +0300

    fixed website link in ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'foundation-learning.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'foundation-nk81.onrender.com']
 
 
 # Application definition
